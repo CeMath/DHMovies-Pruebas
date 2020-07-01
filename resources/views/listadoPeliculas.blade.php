6a912365e40d17d8712fde180f4f287a18d110c4
Mathias <cesar.mathias.arce@gmail.com>
2020-07-01 14:50:17 -0300

    Update HTML

--- resources/views/listadoPeliculas.blade.php
+++ resources/views/listadoPeliculas.blade.php
@@ -6,19 +6,19 @@
 
 @section("principal")
 
-    <h1> Listado de peliculas. </h1>
+@forelse ($peliculas as $pelicula)
+    <div class="container"> 
+    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+        <div class="my-3 py-3">
+        <h2 class="display-5">{{$pelicula["title"]}}</h2>
+        <p class="lead">{{$pelicula["rating"]}}</p>
+        </div>
+        <!-- <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div> -->
+    </div>
+    </div>
+@empty
+    <p> No hay Peliculas </p>
+@endforelse
 
-    <ul>
-        @forelse ($peliculas as $pelicula)
-        <li> 
-            <p> {{$pelicula["title"]}} </p>    
-            @if ($pelicula["rating"] > 8)
-            <p> EXCELENTE </p>
-            @endif
-        </li>
-        @empty
-            <p> No hay peliculas en el listado </p>
-        @endforelse
-    </ul>
     
 @endsection
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -7,7 +7,26 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="public/css/app.css">
+    <link rel="stylesheet" href="public/css/product.css"> 
+  
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -20,9 +39,6 @@
     <header> 
         <nav class="site-header sticky-top py-1">
             <div class="container d-flex flex-column flex-md-row justify-content-between">
-                <a class="py-2" href="#" aria-label="Product">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
-                </a>
                 <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
                 <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
                 <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
