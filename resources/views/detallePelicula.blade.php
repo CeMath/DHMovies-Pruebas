534446a5024f6d51de6c2616fc5201570ea472d4
Mathias <Arce>
2020-07-03 22:27:19 -0300

    as

--- resources/views/detallePelicula.blade.php
+++ resources/views/detallePelicula.blade.php
@@ -7,8 +7,8 @@
 @section("principal")
     <h1> Detalle de la pelicula {{$peliculas["title"]}} </h1>    
     <br>
-
-    <p> Esta pelicula fue estrenada el {{$peliculas["release_date"]}}, 
+    
+    <p> Esta pelicula fue estrenada el {{ date('d-M-Y', strtotime($peliculas["release_date"])) }}, 
         pertenece al genero {{$generoPelicula["name"]}},
         con {{$peliculas["awards"]}} premios ganados 
         y un rating de {{$peliculas["rating"]}}.
